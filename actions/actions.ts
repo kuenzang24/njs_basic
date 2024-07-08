4c304d333cf5ae77a2b34a1093e0dd8a915ade54
kuenzang24.dev <kuenzang24.dev@gmail.com>
2024-07-08 18:42:12 +0600

    added the database and prisma settings

--- actions/actions.ts
+++ actions/actions.ts
@@ -4,6 +4,7 @@ import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
 
 export async function createPost(formData: FormData) {
+  
   const title = formData.get("title") as string;
   const body = formData.get("body") as string;
   console.log(title)
